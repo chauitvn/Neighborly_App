87b5fa0e761d06b710ecc104d76193b2b8a5b4c5
chauitvn <chaunn3502@gmail.com>
2023-09-10 16:51:03 +0800

    update get advertisement function

--- NeighborlyAPI/getAdvertisement/__init__.py
+++ NeighborlyAPI/getAdvertisement/__init__.py
@@ -1,24 +1,32 @@
 import logging
-
+import os
+import pymongo
+from bson.json_util import dumps
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python Get Advertisement function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    id = req.params.get('id')
+    print("--------------->", id)
+
+    if id:
         try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+            url = os.environ['CosmosDBConectionString']
+            client = pymongo.MongoClient(url)
+            database = client['neighbourly_app_db']
+            colection = database['advertisements']
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+            query = {"_id": ObjectId(id)}
+            advertisement = colection.find_one(query)
+            print("----------result--------")
+
+            result = dumps(result)
+            print(result)
+
+            return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+        except ValueError:
+            return func.HttpResponse("Database connection error.", status_code=500)
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse("Please pass an id parameter in the query string.", status_code=400)
